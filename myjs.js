17a5e3f088501e6bf041935035067237b03149d1
Yujin Gu <Yujin@0587389692.wireless.umich.net>
2017-12-03 11:29:28 -0500

    second

---
+++ myjs.js
@@ -0,0 +1,34 @@
+window.onload=function(){ 
+    $('nav a').on('click', function() {
+
+var scrollAnchor = $(this).attr('data-scroll'),
+    scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - 28;
+
+$('body,html').animate({
+    scrollTop: scrollPoint
+}, 500);
+
+return false;
+
+})
+
+
+$(window).scroll(function() {
+    var windscroll = $(window).scrollTop();
+    if (windscroll >= 100) {
+        $('nav').addClass('fixed');
+        $('.wrapper section').each(function(i) {
+            if ($(this).position().top <= windscroll - 20) {
+                $('nav a.active').removeClass('active');
+                $('nav a').eq(i).addClass('active');
+            }
+        });
+
+    } else {
+
+        $('nav').removeClass('fixed');
+        $('nav a.active').removeClass('active');
+        $('nav a:first').addClass('active');
+    }
+
+}).scroll();} 
